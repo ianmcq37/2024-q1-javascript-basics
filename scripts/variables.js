aa160931d22a64cfd1f84fa9bfbc752a573a9652
ianmcq37 <ianmcq37@gmail.com>
2024-08-21 16:19:37 -0600

    variables and stuff

---
+++ scripts/variables.js
@@ -0,0 +1,7 @@
+// this variable is a number
+let intelligenceQuotient = 100;
+
+let firstName = "Goofy";
+let lastName = "Goober";
+
+console.log(firstName, lastName, "has IQ", intelligenceQuotient);
