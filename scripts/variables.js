2c6661298e58083927c174c9c8c32c9aca80bfe6
ianmcq37 <ianmcq37@gmail.com>
2024-08-22 16:23:37 -0600

    variables, objects, and arrays

--- scripts/variables.js
+++ scripts/variables.js
@@ -1,7 +1,42 @@
 // this variable is a number
-let intelligenceQuotient = 100;
+let intelligenceQuotient = 2;
 
+// this is a string, because it has quotes
 let firstName = "Goofy";
 let lastName = "Goober";
 
+// these are constants and cannot be reassigned
+const MIN_IQ = 1;
+const MAX_IQ = 200;
+
+// boolean is true or false
+let isIanTheCoolest = true;
+
+// currently undefined
+let dontKnow; 
+
+// null is a value that has no value, or 0
+let intoTheVoid = null;
+
 console.log(firstName, lastName, "has IQ", intelligenceQuotient);
+
+// this is an object
+let myPet = {
+    type: "Dog",
+    name: "Mischief",
+    age: 12,
+    color: "black:"
+};
+
+console.log ("myPet looks like this", myPet);
+
+// arrays are fun! apparently..
+let favCandies = [
+    "reeses",
+    "snickers",
+    "m&ms",
+]
+
+console.log ("my favorite candies are:", favCandies)
+//arrays start counting from 0
+console.log ("my favorite candies at index 2 is:", favCandies[2]);
