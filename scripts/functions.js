66cc5297579cfdea25e17d43c6b2fcddb239397e
ianmcq37 <ianmcq37@gmail.com>
2024-08-27 16:22:09 -0600

    more functions

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,12 +1,26 @@
 //@ts-check
 
-//let firstName = "Ian";
+let firstName = "Throngler";
 
-let sayHi = function (f, a) {
-    let firstName = f;
-    let age = a;
+let sayHi = function (firstName, age) {
     let greeting = `Hello! my name is ${firstName} and I am ${age} years old.`;
     console.log(greeting);
 };
 
-sayHi();
+sayHi("Ian", 17);
+sayHi(firstName, 21);
+
+function add(a, b) {
+    let answer = a + b;
+    return answer;
+}
+
+console.log(add(100, 5000));
+
+let runMyStuff = (x, y, f) => {
+    let answer = f(x, y);
+    return answer;
+}
+
+runMyStuff("JamesPatrick", 9, sayHi);
+
