b9bab0d81ae6e0edc941a90e302019668c46c96c
ianmcq37 <ianmcq37@gmail.com>
2024-09-03 15:43:01 -0600

    shenanigans

---
+++ scripts/test.js
@@ -0,0 +1,28 @@
+const canvas =document.querySelector('canvas')
+const c = canvas.getContext('2d')
+
+canvas.width = 64*16 //1024
+canvas.height = 64*9 //576
+
+const player = new Player()
+
+//let bottom = y +100
+function animate() {
+    window.requestAnimationFrame(animate)
+    c.fillStyle = 'white'
+    c.fillRect(0, 0, canvas.width, canvas.height)
+    
+    player.draw()
+    player.update()
+}
+
+animate()
+
+window.addEventListener('keydown', (event) => {
+    switch (event.key) {
+    case 'w':
+    player.velocity.y =-20
+    
+    break
+}
+})
