84b3fa93472aa39cf2215b7f31c05cbe2b8e10fe
ianmcq37 <ianmcq37@gmail.com>
2024-09-10 16:22:44 -0600

    more rps stuff yippee

--- scripts/rps-game.js
+++ scripts/rps-game.js
@@ -5,14 +5,10 @@ let btnRock = document.getElementById("btn-rock")
 let btnPaper = document.getElementById("btn-paper")
 let btnScissors = document.getElementById("btn-scissors")
 
-//function rockClicked(rock) {console.log("rock was selected");}
-//btnRock?.addEventListener("click", rockClicked);
-
-//function paperClicked(paper) {console.log("paper was selected");}
-//btnPaper?.addEventListener("click", paperClicked);
-
-//function scissorsClicked(scissors) {console.log("scissors was selected");}
-//btnScissors?.addEventListener("click", scissorsClicked);
+/** @type {HTMLElement} */
+//@ts-ignore We know gameResultHeader is not null
+let gameResultHeader = document.getElementById("game-results")
+gameResultHeader.innerText = "Hey There!"
 
 let isGameOver = false
 
@@ -65,6 +61,7 @@ function playerControlHandler(e) {
 
     let computerWeapon = pickRandomWeapon(weapons)
     let result = determineOutcome(playerWeapon, computerWeapon)
+    gameResultHeader.innerText = result
     console.log(result)
 
 }
