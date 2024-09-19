1950fb1863df1b6bbe17d88789229269258bc1b0
ianmcq37 <ianmcq37@gmail.com>
2024-09-19 16:19:05 -0600

    colors!

--- scripts/canvas/app.js
+++ scripts/canvas/app.js
@@ -9,6 +9,22 @@ const ctx = canvas.getContext("2d");
 
 ctx.fillRect(0, 0, 50, 50);
 
+ctx.fillStyle = "hsla(0, 0%, 0%, 100%)"
+
 ctx.beginPath();
 ctx.arc(100, 100, 25, 0, Math.PI * 2);
 ctx.fill();
+
+let lastTime = 0;
+let hue = 0;
+
+function drawLoop(timestamp) {
+    let elapsedTime = timestamp - lastTime;
+    lastTime = timestamp;
+    ctx.fillStyle = `hsla(${hue}, 100%, 50%, 100%)`;
+    ctx.fillRect(200, 200, 100, 100);
+    hue = hue + 1;
+    //console.log(elapsedTime);
+    window.requestAnimationFrame(drawLoop);
+}
+window.requestAnimationFrame(drawLoop);
